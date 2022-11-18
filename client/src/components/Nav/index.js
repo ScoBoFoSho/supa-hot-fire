3eb465beb451a412f2c285b648501a70acd669d4
Cisco Ferre <ferrecisco@icloud.com>
2022-11-17 19:23:41 -0500

    copy repo from team repo

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,57 @@
+import React from "react";
+import Auth from "../../utils/auth";
+// import { TOGGLE_MODAL } from "../../utils/actions";
+import { Link } from "react-router-dom";
+import logo from "./../../assets/SHFSW.png"
+function Nav() {
+
+  function showNavigation() {
+    if (Auth.loggedIn()) {
+      return (
+        <ul className="flex-row">
+          <li className="mx-1">
+            <Link to="/orderHistory">
+              Order History
+            </Link>
+          </li>
+          <li className="mx-1">
+            {/* this is not using the Link component to logout or user and then refresh the application to the start */}
+            <a href="/" onClick={() => Auth.logout()}>
+              Logout
+            </a>
+          </li>
+        </ul>
+      );
+    } else {
+      return (
+        <ul className="flex-row">
+          <li className="mx-1 nav-li">
+            <Link to="/signup">
+              Signup
+            </Link>
+          </li>
+          <li className="mx-1 nav-li">
+            <Link to="/login" >
+              Login
+            </Link>
+          </li>
+        </ul>
+      );
+    }
+  }
+
+  return (
+    <header className="flex-row px-1">
+              
+        <Link to="/">
+        <img alt="logo" className="logo"src={logo}></img>
+        </Link>
+
+      <nav>
+        {showNavigation()}
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
